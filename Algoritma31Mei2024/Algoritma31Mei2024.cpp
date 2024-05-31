2eeeb60aa50a1b51eb1e577d8fee0c8e5a8beb04
ZahwaAnritia <anritia15@gmail.com>
2024-05-31 11:05:19 +0700

    menambahkan Iterasi dari awal array hingga REAR

--- Algoritma31Mei2024/Algoritma31Mei2024.cpp
+++ Algoritma31Mei2024/Algoritma31Mei2024.cpp
@@ -83,10 +83,16 @@ public:
 		}
 		else {
 			//jika FRONT_position > REAR_position, iterasi  dari FRONT hingga akhir array
-			while (FRONT_position <== max - 1) {
+			while (FRONT_position <= max - 1) {
 				cout << queue_array[FRONT_position] << " ";
 				FRONT_position++;
 			}
+			FRONT_position = 0;
+
+			//Iterasi dari awal array hingga REAR
+			while (FRONT_position <= REAR_position)
+				cout << queue_array[FRONT_position] << " ";
+			FRONT_position++;
 		}
 	}
 };
