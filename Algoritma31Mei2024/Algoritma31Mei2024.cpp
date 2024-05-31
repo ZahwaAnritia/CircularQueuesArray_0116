54d19eb06360ce6841884179a48c1c8db7563a25
ZahwaAnritia <anritia15@gmail.com>
2024-05-31 10:36:01 +0700

    mengecek apakah antrian kosong

--- Algoritma31Mei2024/Algoritma31Mei2024.cpp
+++ Algoritma31Mei2024/Algoritma31Mei2024.cpp
@@ -21,5 +21,11 @@ public:
 		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
 			cout << "\nQueue overflow\n";
 		}
+		//cek apakah antrian kosong
+		if (FRONT == 1) {
+			FRONT = 0;
+			REAR = 0;
+
+		}
 	}
 };
