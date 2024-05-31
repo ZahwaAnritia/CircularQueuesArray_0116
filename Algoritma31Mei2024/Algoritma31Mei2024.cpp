e3d5b79f8838cfdf0596d23ff3390f55e5ffe96c
ZahwaAnritia <anritia15@gmail.com>
2024-05-31 10:28:34 +0700

    menambahkan akses modifier public

--- Algoritma31Mei2024/Algoritma31Mei2024.cpp
+++ Algoritma31Mei2024/Algoritma31Mei2024.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 
-class queuas {
+class Queues {
 	int FRONT, REAR, max = 5;
 	int queques_array[5];
+
+public:
+	Queues() {
+		FRONT = -1;
+		REAR = -1;
+	}
 };
