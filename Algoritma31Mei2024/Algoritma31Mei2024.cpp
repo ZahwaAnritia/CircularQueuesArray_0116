b52b953f465db8338bf64e07f2150612f03fc600
ZahwaAnritia <anritia15@gmail.com>
2024-05-31 10:34:22 +0700

    mengecek apakah antrian penuh

--- Algoritma31Mei2024/Algoritma31Mei2024.cpp
+++ Algoritma31Mei2024/Algoritma31Mei2024.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Queues {
 	int FRONT, REAR, max = 5;
-	int queques_array[5];
+	int queue_array[5];
 
 public:
 	Queues() {
@@ -16,5 +16,10 @@ public:
 		cout << "Enter a number: ";
 		cin >> num;
 		cout << endl;
+
+		//Cek apakah antrian penuh
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overflow\n";
+		}
 	}
 };
